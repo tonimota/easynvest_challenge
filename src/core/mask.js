4f6d5a45ecc71601b623b8b5a04465d13f512bf7
Tonielton Mota <toni.elton1993@gmail.com>
2021-03-22 14:55:36 -0300

    [FEAT] Create input mask for cpf and phone

---
+++ src/core/mask.js
@@ -0,0 +1,23 @@
+export default class Mask {
+  inputMask() {
+    document.getElementById('phone').addEventListener('input', function (e) {
+      var x = e.target.value
+        .replace(/\D/g, '')
+        .match(/(\d{0,2})(\d{0,5})(\d{0,4})/);
+      e.target.value = !x[2]
+        ? x[1]
+        : '(' + x[1] + ') ' + x[2] + (x[3] ? '-' + x[3] : '');
+    });
+
+    document.getElementById('cpf').addEventListener('input', function (e) {
+      let value = '';
+      value = e.target.value
+        .replace(/\D/g, '')
+        .replace(/(\d{3})(\d)/, '$1.$2')
+        .replace(/(\d{3})(\d)/, '$1.$2')
+        .replace(/(\d{3})(\d{1,2})/, '$1-$2')
+        .replace(/(-\d{2})\d+?$/, '$1');
+      e.target.value = value;
+    });
+  }
+}
